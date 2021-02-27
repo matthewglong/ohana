357636976c89ffe7e043e62c8ebf090e08a3192a
Matthew Long <32203334+matthewglong@users.noreply.github.com>
2021-02-27 14:02:27 -0800

    encrypted sfdc creds

--- login.py
+++ login.py
@@ -4,6 +4,49 @@ import json
 from simple_salesforce import Salesforce
 import os
 import getpass
+from cryptography.fernet import Fernet
+
+
+def LoadSecretKey(filepath=""):
+    if "secret.key" not in os.listdir(filepath):
+        key = Fernet.generate_key()
+        with open(f"{filepath}/secret.key", "wb") as key_file:
+            key_file.write(key)
+    else:
+        with open(f"{filepath}/secret.key", "rb") as key_file:
+            key = key_file.read()
+    secret = Fernet(key)
+    return secret
+
+
+def EncryptText(text, secret):
+    encoded_text = text.encode()
+    encrypted_bits = secret.encrypt(encoded_text)
+    encrypted_text = encrypted_bits.decode()
+    return encrypted_text
+
+
+def DecryptText(text, secret):
+    encoded_text = text.encode()
+    decrypted_bits = secret.decrypt(encoded_text)
+    decrypted_text = decrypted_bits.decode()
+    return decrypted_text
+
+
+def EncryptDict(target_dict, secret_key):
+    encrypted_dict = {
+        EncryptText(i, secret_key): EncryptText(v, secret_key)
+        for i, v in target_dict.items()
+    }
+    return encrypted_dict
+
+
+def DecryptDict(target_dict, secret_key):
+    decrypted_dict = {
+        DecryptText(i, secret_key): DecryptText(v, secret_key)
+        for i, v in target_dict.items()
+    }
+    return decrypted_dict
 
 
 def InstSFDC():
@@ -16,10 +59,12 @@ def InstSFDC():
     if "sfdc_creds" not in os.listdir(root):
         os.mkdir(creds_dir)
 
+    secret_key = LoadSecretKey(creds_dir)
+
     try:
         with open(creds_path) as c:
-            creds = json.load(c)
-
+            encrypted_creds = json.load(c)
+            creds = DecryptDict(encrypted_creds, secret_key)
             sf = Salesforce(
                 username=creds["username"],
                 password=creds["password"],
@@ -41,8 +86,10 @@ def InstSFDC():
             else:
                 creds[key] = input(help_text)
 
+        encrypted_creds = EncryptDict(creds, secret_key)
+
         with open(creds_path, "w") as outfile:
-            json.dump(creds, outfile)
+            json.dump(encrypted_creds, outfile)
 
         sf = InstSFDC()
 
